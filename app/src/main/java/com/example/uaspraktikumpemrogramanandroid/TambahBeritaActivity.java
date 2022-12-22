eabeff747c2afc6ca36e4b610a03adec994ad9f5
AdikaDS <adika.dwi.saputra@mail.ugm.ac.id>
2022-12-22 19:05:23 +0700

    new feature

---
+++ app/src/main/java/com/example/uaspraktikumpemrogramanandroid/TambahBeritaActivity.java
@@ -0,0 +1,15 @@
+package com.example.uaspraktikumpemrogramanandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TambahBeritaActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_berita);
+    }
+}
